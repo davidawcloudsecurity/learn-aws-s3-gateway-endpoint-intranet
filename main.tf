7c2f16b5c64f3dccd83c21786e193e6f36474c09
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-02-15 21:21:09 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -84,7 +84,6 @@ resource "aws_s3_bucket" "static_website" {
 resource "aws_vpc_endpoint" "s3" {
   vpc_id       = aws_vpc.main.id
   service_name   = "com.amazonaws.${var.region}.s3"
-  route_table_ids = [aws_route_table.rt.id]
 
   tags = var.tags
 }

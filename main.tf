2ab0aa87a1e9d0dcb8b4a9421058a7dd82dee0b7
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-02-17 08:09:15 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,6 +31,8 @@ variable "tags" {
 
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr_block
+  enable_dns_support = true
+  enable_dns_hostnames = true
 
   tags = merge(var.tags, {
     Name = "vpc-${var.tags["Environment"]}"

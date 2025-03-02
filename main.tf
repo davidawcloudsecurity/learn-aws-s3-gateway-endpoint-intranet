c80c422a35ef4f7f36f4fe81c8f4d9159e4938ec
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-03-02 16:12:36 +0800

    Update main.tf - get the ip address of the subnet from vpc s3 endpoint

--- main.tf
+++ main.tf
@@ -340,20 +340,11 @@ resource "aws_lb_listener" "http" {
 }
 
 resource "aws_lb_target_group" "tg" {
-  name     = "my-target-group"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
-
-  target {
-    id   = aws_subnet.main.id
-    port = 80
-  }
-
-  target {
-    id   = aws_subnet.second_subnet.id
-    port = 80
-  }
+  name        = "my-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
 
   health_check {
     path                = "/"
@@ -361,8 +352,38 @@ resource "aws_lb_target_group" "tg" {
     unhealthy_threshold = 3
     timeout             = 6
     interval            = 30
-    matcher             = "307,405"  # Check for these status codes
+    matcher             = "307,405"
   }
+}
+
+# Get the network interfaces associated with the S3 VPC endpoint
+data "aws_network_interfaces" "s3_endpoint_enis" {
+  filter {
+    name   = "vpc-id"
+    values = [aws_vpc.main.id]
+  }
+  
+  filter {
+    name   = "group-id"
+    values = [aws_security_group.ec2_sg.id]
+  }
+  
+  filter {
+    name   = "description"
+    values = ["*com.amazonaws.${var.region}.s3*"]
+  }
+
+  depends_on = [aws_vpc_endpoint.s3_interface]
+}
+
+# Dynamically create target group attachments for each endpoint ENI
+resource "aws_lb_target_group_attachment" "s3_endpoint_targets" {
+  count            = length(data.aws_network_interfaces.s3_endpoint_enis.ids)
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = tolist(data.aws_network_interfaces.s3_endpoint_enis.private_ips)[count.index]
+  port             = 80
+
+  depends_on = [aws_vpc_endpoint.s3_interface]
 }
 
 resource "aws_lb_listener_rule" "trailing_slash_redirect" {

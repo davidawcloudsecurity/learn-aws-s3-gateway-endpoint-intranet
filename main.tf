a693abdd97b16b780399aad25985d4d9ab4fb57a
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-03-02 15:59:18 +0800

    Update main.tf - include endpoint for ssm

--- main.tf
+++ main.tf
@@ -64,6 +64,42 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
+resource "aws_vpc_endpoint" "ssm_interface" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${var.region}.ssm"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  subnet_ids          = [aws_subnet.main.id, aws_subnet.second_subnet.id]
+  security_group_ids  = [aws_security_group.ec2_sg.id]
+
+  tags = var.tags
+  depends_on = [aws_vpc.main]
+}
+
+resource "aws_vpc_endpoint" "ssm_ec2" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${var.region}.ec2messages"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  subnet_ids          = [aws_subnet.main.id, aws_subnet.second_subnet.id]
+  security_group_ids  = [aws_security_group.ec2_sg.id]
+
+  tags = var.tags
+  depends_on = [aws_vpc.main]
+}
+
+resource "aws_vpc_endpoint" "ssm_messages" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${var.region}.ssmmessages"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  subnet_ids          = [aws_subnet.main.id, aws_subnet.second_subnet.id]
+  security_group_ids  = [aws_security_group.ec2_sg.id]
+
+  tags = var.tags
+  depends_on = [aws_vpc.main]
+}
+
 resource "aws_vpc_endpoint" "s3_interface" {
   vpc_id              = aws_vpc.main.id
   service_name        = "com.amazonaws.${var.region}.s3"
